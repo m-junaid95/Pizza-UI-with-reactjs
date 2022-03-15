491c787a280abe3bc2914eed9266e2af9883c9d4
Junaid <zulfiqar.junaid95@gmail.com>
2022-03-16 00:52:15 +0500

    My First Commit

---
+++ src/components/Clients.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Clients = () => {
+  return (
+    <div className="clients">
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <div className="client">
+              <h1>210</h1>
+              <p>Awesome Employees</p>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="client">
+              <h1>45</h1>
+              <p>Pizza Types</p>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="client">
+              <h1>19802</h1>
+              <p>Satisfied Clients</p>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="client">
+              <h1>1922</h1>
+              <p>Delivery</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Clients;
